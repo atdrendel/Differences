4f520b7d4017e9c8ef9889486613856bf9299893
Anthony Drendel <anthony@goneeast.com>
2016-12-19 15:41:42 -0600

    Add Collection+Differences and Differences

--- Tests/DifferencesTests/DifferencesTests.swift
+++ Tests/DifferencesTests/DifferencesTests.swift
@@ -2,16 +2,17 @@ import XCTest
 @testable import Differences
 
 class DifferencesTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(Differences().text, "Hello, World!")
-    }
-
-
     static var allTests : [(String, (DifferencesTests) -> () throws -> Void)] {
         return [
             ("testExample", testExample),
         ]
+    }
+
+    func testExample() {
+        let old = [0, 1, 2]
+        let new = [2, 1, 3]
+
+        XCTAssertEqual([Difference.update(0)], old.differences(to: new))
+        XCTAssertEqual([Difference.update(0)], new.differences(from: old))
     }
 }
